19e59e5552b4bff758347fb93de53c90668f5d80
Sergey Gavrilyuk <sergey.gavrilyuk@gmail.com>
2013-01-13 23:29:01 -0500

    Initial Commit

---
+++ Projects/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Projects
+//
+//  Created by Sergey Gavrilyuk on 13-01-13.
+//  Copyright (c) 2013 Sergey Gavrilyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRJAppDelegate class]));
+    }
+}
